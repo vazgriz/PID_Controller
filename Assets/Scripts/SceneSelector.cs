6c25763c8f44c4184e10a886bf12066f1331a251
Ryan Vazquez <contact@ryan-vazquez.dev>
2021-12-21 14:04:53 -0600

    Add SceneSelector

---
+++ Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour {
+    [SerializeField]
+    int currentScene;
+
+    public void LoadNextLevel() {
+        SceneManager.LoadScene(currentScene + 1);
+    }
+
+    public void LoadLastLevel() {
+        SceneManager.LoadScene(currentScene - 1);
+    }
+}
